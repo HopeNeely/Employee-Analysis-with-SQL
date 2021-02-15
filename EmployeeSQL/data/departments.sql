4a0cdb475ed5b3028fbd18d22fab7c3f26714472
HopeNeely <hope.neely@gmail.com>
2021-02-15 10:52:36 -0500

    moved ERD.png into Data directory and saved departments.sql querie.

---
+++ EmployeeSQL/data/departments.sql
@@ -0,0 +1,12 @@
+-- First Table: departments
+
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments
+(
+    dept_no VARCHAR(30) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
+	);
+
+-- Then import data from departments.csv by right-clicking the table and using import/export tool.  
