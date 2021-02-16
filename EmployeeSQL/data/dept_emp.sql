2d175998b426b65e6d8c9c232dc94a737b350de1
HopeNeely <hope.neely@gmail.com>
2021-02-16 09:38:42 -0500

    Cleaned up 6th table, dept_emp schemas to create compound primary key.

--- EmployeeSQL/data/dept_emp.sql
+++ EmployeeSQL/data/dept_emp.sql
@@ -1,20 +1,12 @@
 -- 6th Table: dept_emp
 
-DROP TABLE IF EXISTS dept_emp;
+-- DROP TABLE IF EXISTS dept_emp;
 
 CREATE TABLE dept_emp
 (
     emp_no INTEGER,
-    dept_no VARCHAR(30)
+    dept_no VARCHAR(30),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Then import data from dept_emp.csv by right-clicking the table and using import/export tool.  
-
-ALTER TABLE dept_emp ADD COLUMN id SERIAL PRIMARY KEY;
-
-ALTER TABLE dept_emp
-	ADD CONSTRAINT add_fk
-	FOREIGN KEY (emp_no) 
-	REFERENCES salaries(emp_no);
-
-SELECT * FROM dept_emp;
