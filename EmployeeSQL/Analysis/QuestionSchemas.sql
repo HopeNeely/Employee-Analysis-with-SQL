70c3ad82203371a8387a781df2955ecfa4e7f1c0
HopeNeely <hope.neely@gmail.com>
2021-02-17 09:31:48 -0500

    Answered question 2.

--- EmployeeSQL/Analysis/QuestionSchemas.sql
+++ EmployeeSQL/Analysis/QuestionSchemas.sql
@@ -1,17 +1,35 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 JOIN salaries
   ON employees.emp_no = salaries.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE to_char(hire_date, 'YYYY-MM-DD') LIKE '1986%'; 
 
+-- 3. List the manager of each department with the following information: 
+--         department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM employees
+JOIN dept_manager
+  ON dept_manager.emp_no = employees.emp_no
+JOIN departments
+  ON departments.dept_no = dept_manager.dept_no;
 
-
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4. List the department of each employee with the following information: 
+--          employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_manager
+  ON dept_manager.emp_no = employees.emp_no
+JOIN departments
+  ON departments.dept_no = dept_manager.dept_no
+JOIN salaries
+  ON employees.emp_no = salaries.emp_no
+JOIN dept_emp
+  ON salaries.emp_no = dept_emp.emp_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
